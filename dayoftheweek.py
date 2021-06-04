091e2e974563f375dfee564c5250f7fd40d3771d
Dylan Guidry <dylanguidry20@gmail.com>
2021-06-04 09:42:58 -0500

    some modificatioins to day file

--- dayoftheweek.py
+++ dayoftheweek.py
@@ -1,22 +1,24 @@
 #Input for user
 print('Please choose a number between 0 and 6 below... ')
-day = int(input('(0-6)? '))
 
 #Numeric for each day of the week
-if (day) == 0:
-    print("Sunday")
-if (day) == 1:
-    print("Monday")
-if (day) == 2:
-    print("Tuesday")
-if (day) == 3:
-    print("Wednesday")
-if (day) == 4:
-    print("Thursday")
-if (day) == 5:
-    print("Friday")
-if (day) == 6:
-    print("Saturday")
+try:
+    day = int(input('(0-6)? '))
 
-#Print at the end!
-print("You passed the 3rd grade!")
+    if (day) == 0:
+        print("Sunday")
+    elif (day) == 1:
+        print("Monday")
+    elif (day) == 2:
+        print("Tuesday")
+    elif (day) == 3:
+        print("Wednesday")
+    elif (day) == 4:
+        print("Thursday")
+    elif (day) == 5:
+        print("Friday")
+    elif (day) == 6:
+        print("Saturday")
+    else:
+        print("Please insert a listed value.")
+except ValueError: print("Invalid.")
